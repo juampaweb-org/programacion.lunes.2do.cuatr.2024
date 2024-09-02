83dacad74610b69625e4a189a04ac8322f3d740d
Juampa <juanpablososa@gmail.com>
2024-09-02 20:36:10 -0300

    Unidad 3, modifico script de sentencias if

--- unidad-03/en_clase/06-sentencias_if.py
+++ unidad-03/en_clase/06-sentencias_if.py
@@ -11,13 +11,22 @@ alumno_edad = int(input("Ingrese la edad del alumno: "))
 
 
 # Anidamientos de if / else
+
 if alumno_edad > 40:
-    print("El alumno", alumno_nombre, "es mayor de 40 años.")
+    mens_salida = "El alumno es mayor a 40 años"
+
+elif alumno_edad > 30:
+    mens_salida = "El alumno es mayor a 30 años"
+
+elif alumno_edad > 20:
+    mens_salida = "El alumno es mayor a 20 años"
+
 else:
-    if alumno_edad > 30:
-        print("El alumno", alumno_nombre, "es mayor de 30 años.")
-    else:
-        print("El alumno", alumno_nombre, "es menor de 30 años.")
+    mens_salida = "El alumno es menor a 20 años"
+
+
+print(mens_salida)
+
 
 print("fin del programa")
 print("sigo acá....")
