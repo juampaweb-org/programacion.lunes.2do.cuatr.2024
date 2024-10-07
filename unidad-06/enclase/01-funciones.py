12f7885d5712b682be212ad955ddb1c1c7f3a739
Juampa <juanpablososa@gmail.com>
2024-10-07 16:22:47 -0300

    Parte Unidad 6

--- unidad-06/enclase/01-funciones.py
+++ unidad-06/enclase/01-funciones.py
@@ -1,15 +1,20 @@
 
 
-
-def sumar_dos_numeros( nro_uno, nro_dos):
+# docstring sirve para documentar la función
+def sumar_dos_numeros( nro_uno = 0, nro_dos = 0):
+    """
+    Esta función recibe dos números y retorna la suma de ellos
+    param nro_uno: int
+    param nro_dos: int
+    return: int
+    """
+    if nro_dos == 0:
+        print( "No se ha ingresado el segundo número" )
+        return "Error"
+    # inicio del blque de la función
     total = nro_uno + nro_dos
     return total
 
-
-
-total_suma = sumar_dos_numeros(10, 20)
-
-print("El total de la suma es: ", total_suma)
 
 
 
